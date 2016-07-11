48f265211ef59b7d0cfb99fbd1425abaa2bfc424
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2016-07-11 15:05:52 +0200

    WSAvcPlayer can now receive a connect SocketIO client instead of a WebSocket URI

--- public/main.js
+++ public/main.js
@@ -4,9 +4,15 @@ var WSAvcPlayer = require('../vendor');
 var canvas = document.createElement("canvas");
 document.body.appendChild(canvas);
 // Create h264 player
-var uri = "ws://" + document.location.host;
+
+// var uri = "ws://" + document.location.host;
+// var wsavc = new WSAvcPlayer(canvas, "webgl", 1, 35);
+// wsavc.connect(uri);
+
+var socketIOURL = location.protocol + '//' + document.domain + ':' + location.port + '/h264';
+var client = io.connect(socketIOURL, {path: '/'});
 var wsavc = new WSAvcPlayer(canvas, "webgl", 1, 35);
-wsavc.connect(uri);
+wsavc.connect(client);
 
 
 //for button callbacks
